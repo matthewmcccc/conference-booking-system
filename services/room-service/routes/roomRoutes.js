bd108d2a3918af42836a956bc25e60ad153ca5e9
matthewmcccc <xyzmonte@protonmail.com>
2025-12-21 17:23:10 +0000

    move room health route above parameterized routes

--- services/room-service/routes/roomRoutes.js
+++ services/room-service/routes/roomRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const roomController = require("../controllers/roomController");
 
+router.get("/health", roomController.health);
 router.get("/", roomController.getAllRooms);
 router.get("/:id", roomController.getRoomById);
 router.put("/:id", roomController.editRoomById);
 router.post("/", roomController.createRoom);
 router.delete("/:id", roomController.deleteRoom)
 router.delete("/location/:locationId", roomController.deleteRoomByLocationId);
-router.get("/health", roomController.health);
 
 module.exports = router;
