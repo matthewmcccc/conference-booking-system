0ef531770c134775b66ffb3a812460d53e150358
matthewmcccc <xyzmonte@protonmail.com>
2025-12-21 16:48:02 +0000

    move health route before parameterized routes

--- services/location-service/routes/locationRoutes.js
+++ services/location-service/routes/locationRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const locationController = require('../controllers/locationController');
 
+router.get("/health", locationController.health);
 router.get('/', locationController.getAllLocations);
 router.post('/', locationController.createNewLocation);
 router.get('/:id', locationController.getLocationById);
 router.get('/:id/rooms', locationController.getAllRoomsForLocation);
 router.put('/:id', locationController.editLocationById)
 router.delete('/:id', locationController.deleteLocation);
-router.get("/health", locationController.health);
 
 module.exports = router;
